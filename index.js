cea2c0635b637a82ef005fb615ec965f9151d550
Michaela Wagner <mickywagner@gmail.com>
2020-12-11 03:01:21 -0700

    Update question prompt with axios call to db

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 #! /usr/bin/env node
+
+const { default: Axios } = require('axios');
+
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout,
 })
 
-readline.question('What would you like to log today?', item => {
+readline.question('What would you like to log today?', async (item) => {
+    const { data } = await Axios.get(`http://localhost:3001/food`)
+    const it = data[Symbol.iterator]();
+    
     console.log(item);
     readline.close()
 })
