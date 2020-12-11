8ccadf43c049854b9b1eb4df4265c9bc08a91212
Michaela Wagner <mickywagner@gmail.com>
2020-12-11 03:08:43 -0700

    Update prompt with iterable

--- index.js
+++ index.js
@@ -9,8 +9,16 @@ const readline = require('readline').createInterface({
 
 readline.question('What would you like to log today?', async (item) => {
     const { data } = await Axios.get(`http://localhost:3001/food`)
-    const it = data[Symbol.iterator]();
     
-    console.log(item);
+    const it = data[Symbol.iterator]();
+    let position = it.next()
+    while(!position.done) {
+        const food = position.value.name
+        if (food === item) {
+            console.log(`${item} has ${position.value.calories} calories`)
+        }
+        position = it.next();
+    }
+    
     readline.close()
 })
