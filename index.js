907dfde36d63fa85e341924c2f0a84746bc3f46b
Michaela Wagner <mickywagner@gmail.com>
2020-12-11 03:23:08 -0700

    Add switch statement to allow multiple commands

--- index.js
+++ index.js
@@ -5,20 +5,35 @@ const { default: Axios } = require('axios');
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout,
+    prompt: 'enter comman > '
 })
 
-readline.question('What would you like to log today?', async (item) => {
-    const { data } = await Axios.get(`http://localhost:3001/food`)
-    
-    const it = data[Symbol.iterator]();
-    let position = it.next()
-    while(!position.done) {
-        const food = position.value.name
-        if (food === item) {
-            console.log(`${item} has ${position.value.calories} calories`)
-        }
-        position = it.next();
+readline.prompt();
+
+readline.on('line', line => {
+    switch (line.trim()) {
+        case 'list vegan food' :
+            {
+                console.log('vegan food list')
+            }
+            break;
+        case 'log' :
+            {
+                readline.question('What would you like to log today?', async (item) => {
+                    const { data } = await Axios.get(`http://localhost:3001/food`)
+                    
+                    const it = data[Symbol.iterator]();
+                    let position = it.next()
+                    while(!position.done) {
+                        const food = position.value.name
+                        if (food === item) {
+                            console.log(`${item} has ${position.value.calories} calories`)
+                        }
+                        position = it.next();
+                    }
+                    
+                    readline.prompt()
+                })
+            }
     }
-    
-    readline.close()
 })
