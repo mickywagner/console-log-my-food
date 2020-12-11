477f461c5ff0bdad9807e5ce8bfca61ccb42b353
Michaela Wagner <mickywagner@gmail.com>
2020-12-11 15:31:23 -0700

    Create food log case

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ readline.on("line", async (line) => {
         readline.prompt();
       }
       break;
-    case "log": {
+    case "log": 
       const { data } = await Axios.get(`http://localhost:3001/food`);
       const it = data[Symbol.iterator]();
       let actionIt;
@@ -103,10 +103,44 @@ readline.on("line", async (line) => {
           }
           position = it.next();
         }
-
         readline.prompt();
       });
       break;
+    case `today's log`:
+        readline.question('Email ', async emailAddress => {
+            const { data } = await Axios.get(
+                `http://localhost:3001/users?email=${emailAddress}`
+            )
+            const foodLog = data[0].log || []
+            let totalCalories = 0
+
+            function* getFoodLog() {
+                yield* foodLog
+            }
+            
+            for (const entry of getFoodLog()) {
+                const timestamp = Object.keys(entry)[0]
+                if(isToday(new Date(Number(timestamp)))) {
+                    console.log(
+                        `${entry[timestamp].food}, ${entry[timestamp].servingSize} serving(s)`,
+                    )
+                    totalCalories += entry[timestamp].calories
+                }
+            }
+            console.log('----------------------')
+            console.log(`Total Calories: ${totalCalories}`)
+            readline.prompt()
+        })
+        break;
     }
   }
-});
+);
+
+function isToday(timestamp) {
+    const today = new Date()
+    return (
+        timestamp.getDate() === today.getDate() &&
+        timestamp.getMonth() === today.getMonth() &&
+        timestamp.getFullYear() === today.getFullYear()
+    )
+}
